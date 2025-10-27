b961d55884cefa63af8fe73d251e50d73a934a76
Daniel <tvland140@gmail.com>
2025-10-27 02:20:40 -0500

    Cambios Finales- Proyecto Terminado

--- src/Main4.java
+++ src/Main4.java
@@ -1,10 +1,9 @@
-//ESTE MAIN VA A SERVIR PARA VER LA CREACION DEL ARCHIVOOO
+//ESTE MAIN VA A SERVIR PARA LA PROBAR LA EJECUCION DE LA APP
 
 public class Main4 {
     public static void main (String[]args) {
         App aplicacion= new App();
         aplicacion.ejecutar();
-
 
     }
 }
